8cbf3b0d38b250ab8fc2035e991947a36553741e
Alf Alexander <alf1337@gmail.com>
2015-08-31 07:12:31 -0700

    Create ManyGames_GUI, it looks nice.

--- TDT4113/run.py
+++ TDT4113/run.py
@@ -5,15 +5,20 @@ from random_player import RandomPlayer
 from seq_player import SeqPlayer
 from most_common_player import MostCommonPlayer
 from historian_player import HistorianPlayer
+from manygames_gui import ManyGames_GUI
+
 
 from gui import GUITournament
 from tkinter import Tk, BOTH, StringVar
 
-# Styrer spiller gjennom Tkinter/GUI.
-root = Tk()
-# Definer et vindu med gitte dimensjoner
-root.geometry("1100x500+300+300")
-# Lag instans, og kjoer oppsett av GUI (knapper etc)
-GUITournament(root, HistorianPlayer(3)).setup_gui()
-# Vis vindu, og utfoer tilhoerende kommandoer
-root.mainloop()
+## Styrer spiller gjennom Tkinter/GUI.
+#root = Tk()
+## Definer et vindu med gitte dimensjoner
+#root.geometry("1100x500+300+300")
+## Lag instans, og kjoer oppsett av GUI (knapper etc)
+#GUITournament(root, HistorianPlayer(3)).setup_gui()
+## Vis vindu, og utfoer tilhoerende kommandoer
+#root.mainloop()
+
+play = ManyGames_GUI(HistorianPlayer(2), HistorianPlayer(3), 2000)
+play.play_tournament()
